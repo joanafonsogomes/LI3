4eba84951bdce1f406719dbc3f1787b386d681f4
SusanaMarques <47987683+SusanaMarques@users.noreply.github.com>
2019-03-25 15:10:42 +0000

    Add files via upload

---
+++ Vendas.h
@@ -0,0 +1,16 @@
+#ifndef VALVEND_H_
+#define VALVEND_H_
+
+typedef struct venda* Venda;
+
+char** val ();
+int numero_val();
+char** produto1();
+float* preco1();
+int* quant1();
+char* promonorm1();
+int* mes1();
+int* filial1();
+void Vendas_1MValidas ();
+
+#endif
---
+++ validaProdutoseClientes.h
@@ -0,0 +1,9 @@
+#ifndef VALPRODU_CLIENT_H_
+#define VALPRODU_CLIENT_H_
+
+char** validaProdutos () ;
+int numero_prod ();
+char** validaClientes ();
+int numero_client ();
+
+#endif
