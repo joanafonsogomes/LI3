59c2ea32495b30075150b43b00cd2b92e04db97f
SusanaMarques <47987683+SusanaMarques@users.noreply.github.com>
2019-06-03 18:19:17 +0100

    Delete InterfGereVendasController.java

--- InterfGereVendasController.java
+++
@@ -1,9 +0,0 @@
-
-package liprojeto;
-
-
-public interface InterfGereVendasController {
-    void setModel (InterfGereVendasModel m);
-    void setView(InterfGereVendasView v);
-    void startController();
-}
